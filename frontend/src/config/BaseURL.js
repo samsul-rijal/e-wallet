504d0348e525ee84c1b7dcd3447454470b41aa67
Samsul Rijal <72549004+samsul-rijal@users.noreply.github.com>
2022-07-17 00:04:15 +0700

    Delete frontend directory

--- frontend/src/config/BaseURL.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const baseURL = axios.create({
-    baseURL: "https://sr-wallet-api.herokuapp.com/api/v1/"
-})
--- frontend/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Login from "./Login";
-import Register from "./Register";
-import Profile from "./Profile";
-import Laporan from "./Laporan";
-
-export {Login, Register, Profile, Laporan}
